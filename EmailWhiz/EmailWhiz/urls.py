e10d24a242d4ab8979b7602bca0ab1377a51b97b
Monisha Krishnamurthy <rudrakshiitd@outlook.com>
2024-11-09 17:06:43 -0500

    feat: make login as default page

--- EmailWhiz/EmailWhiz/urls.py
+++ EmailWhiz/EmailWhiz/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-import  emailwhiz_ui
+from django.shortcuts import redirect
 from django.urls import path, include
-from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ui/', include('emailwhiz_ui.urls')),
+    path('', lambda request: redirect('login'))
 ]
--- EmailWhiz/emailwhiz_ui/urls.py
+++ EmailWhiz/emailwhiz_ui/urls.py
@@ -1,9 +1,7 @@
-# myapp/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('add_resume/', views.add_resume, name='resume_form'),
-    # Add more URL patterns as needed
-    path('login/', views.login_view, name='login')
+    path('login/', views.login_view, name='login'),
 ]
